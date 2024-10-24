439de1cb32437975f413c7a44e2c868f197e86db
benpaoxiyang <52010393+benpaoxiyang@users.noreply.github.com>
2024-10-24 21:53:28 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["hr15pm809293.vicp.fun"];
+pub const PUBLIC_RS_PUB_KEY: &str = "e9f68308eab81e5bd9d86a478186428fd481653c06cd56535fbc1515efd2134e";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
